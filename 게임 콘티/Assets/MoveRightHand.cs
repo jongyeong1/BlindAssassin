5f9caf2fcef81c43f499a07d32a9285e2624a8bd
youns1998 <49389722+youns1998@users.noreply.github.com>
2019-11-21 12:51:42 +0900

    초기 그래픽 데이터

---
+++ 게임 콘티/Assets/MoveRightHand.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+public class MoveRightHand: MonoBehaviour
+{
+    public GameObject hands;
+    void Update()
+    {
+
+
+
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+            hands.SetActive(true);
+
+
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+
+            hands.SetActive(false);
+
+
+        }
+
+
+    }
+}
