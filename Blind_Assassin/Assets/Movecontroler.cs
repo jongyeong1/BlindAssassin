faa714f9e05c5cfe613315b132604d8fdbe8110a
jongyeong1 <49389255+jongyeong1@users.noreply.github.com>
2019-12-14 18:01:10 +0900

    캐릭터ver2.0
    라운드별 무브 변경

--- Blind_Assassin/Assets/Movecontroler.cs
+++ Blind_Assassin/Assets/Movecontroler.cs
@@ -9,14 +9,14 @@ public class Movecontroler : MonoBehaviour
     static public int action = 0;
     [SerializeField]
     CharacterController cc;
-
+    
     public float speed = 3.0F;
     public float jumpSpeed = 4.0F;
     public float gravity = 9.81F;
 
     public int Action = 0;      //행동    0은 가만히 1은 왼막 2는 오른막 3은 웅크림
     public int hp = 3;          //체력
-
+  
     private Vector3 moveDirection = Vector3.zero;
     void Update()
     {
@@ -24,12 +24,16 @@ public class Movecontroler : MonoBehaviour
         {
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
+            moveDirection.z *= speed;
             if (Input.GetButton("Jump"))
                 moveDirection.y = jumpSpeed;
 
         }
         moveDirection.y -= gravity * Time.deltaTime;
+
+        moveDirection+=new Vector3(0, 0, 30f);
+   
+
         cc.Move(moveDirection * Time.deltaTime);
     }
 }
